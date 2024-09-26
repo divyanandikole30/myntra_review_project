b3ab3f18c934a1f24d27b2388b1fca8149c1a9dc
divyanandikole30 <divnandikole2@gmail.com>
2024-09-26 23:58:51 +0530

    project upload

---
+++ src/constants/__init__.py
@@ -0,0 +1,5 @@
+MONGODB_URL_KEY: str = "mongodb+srv://Divya29:Nandikole123@cluster0.eqiou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
